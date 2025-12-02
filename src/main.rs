750477d40f3c82c2b2ac21cf1b34b7b099c42b15
Patrick Mollman <pkmollman@gmail.com>
2025-12-02 13:54:30 -0500

    idk

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,16 @@ mod d2;
 use std::fs;
 
 fn load_puzzle_input(file_name: &str) -> String {
-    fs::read_to_string(format!("./puzzle_inputs/{}", file_name)).unwrap()
+    println!("==== DAY {} ====", file_name);
+    fs::read_to_string(format!("./puzzle_inputs/{}.txt", file_name)).unwrap()
 }
 
 fn main() {
-    println!("==== DAY 1 ====");
-    let day_1 = d1::PuzzleResult::process_input(load_puzzle_input("1.txt"), 50);
+    let day_1 = d1::PuzzleResult::process_input(load_puzzle_input("1"), 50);
     println!("times at zero: {}", day_1.times_reached_0);
     println!("times passed zero: {}", day_1.times_passed_0);
 
-    println!("==== DAY 2 ====");
-    let day_2 = d2::PuzzleResult::process_input(load_puzzle_input("2.txt"));
+    let day_2 = d2::PuzzleResult::process_input(load_puzzle_input("2"));
     println!("invalid IDs: {}", day_2.invalid_ids.iter().sum::<i64>());
     println!("invalid IDs 2: {}", day_2.invalid_ids_2.iter().sum::<i64>());
 }
