747769546608c60654f081ffbe5c295e92e3e818
Patrick Mollman <pkmollman@gmail.com>
2025-12-03 13:53:49 -0500

    generic solution

--- src/d3.rs
+++ src/d3.rs
@@ -19,53 +19,41 @@ impl PuzzleResult {
                 .collect();
 
             // PART 1
-            let mut front: u64 = 0;
-            let mut front_index: usize = 0;
-            let mut back: u64 = 0;
-
-            for i in 0..joltage_values.len() {
-                if joltage_values[i] > front && i < joltage_values.len() - 1 {
-                    front = joltage_values[i];
-                    front_index = i;
-                }
-            }
-
-            for i in 0..joltage_values.len() {
-                if joltage_values[i] > back && i > front_index {
-                    back = joltage_values[i];
-                }
-            }
-
-            result.joltages.push(front * 10 + back);
+            result.joltages.push(process_joltage(2, &joltage_values));
 
             // PART 2
-            let mut found_values: Vec<u64> = Vec::new();
-            let mut scan_index: usize = 0;
-
-            for i in 0..12 {
-                let mut remaining_front_slice: Vec<u64> = joltage_values
-                    .iter()
-                    .skip(scan_index)
-                    .take(joltage_values.len() - scan_index - (12 - i) + 1)
-                    .copied()
-                    .collect();
-                remaining_front_slice.sort();
-                found_values.push(remaining_front_slice.last().unwrap().to_owned());
-                for ia in scan_index..joltage_values.len() {
-                    if &joltage_values[ia] == found_values.last().unwrap() {
-                        scan_index = ia + 1;
-                        break;
-                    }
-                }
-            }
-
-            let mut total: u64 = 0;
-            for i in 0..found_values.len() {
-                total += found_values[i] * u64::pow(10, (found_values.len() - i - 1) as u32);
-            }
-            result.joltages_2.push(total);
+            result.joltages_2.push(process_joltage(12, &joltage_values));
         }
 
         return result;
     }
+}
+
+fn process_joltage(target_batteries: usize, joltage_values: &Vec<u64>) -> u64 {
+    let mut found_values: Vec<u64> = Vec::new();
+    let mut scan_index: usize = 0;
+
+    for i in 0..target_batteries {
+        let mut remaining_front_slice: Vec<u64> = joltage_values
+            .iter()
+            .skip(scan_index)
+            .take(joltage_values.len() - scan_index - (target_batteries - i) + 1)
+            .copied()
+            .collect();
+        remaining_front_slice.sort();
+        found_values.push(remaining_front_slice.last().unwrap().to_owned());
+        for ia in scan_index..joltage_values.len() {
+            if &joltage_values[ia] == found_values.last().unwrap() {
+                scan_index = ia + 1;
+                break;
+            }
+        }
+    }
+
+    let mut total: u64 = 0;
+    for i in 0..found_values.len() {
+        total += found_values[i] * u64::pow(10, (found_values.len() - i - 1) as u32);
+    }
+
+    return total;
 }
